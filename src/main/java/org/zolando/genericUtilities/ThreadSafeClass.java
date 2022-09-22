a87449bcfdd6dfbb899623bb7e07fb106513409f
PREM <PREM@PREM-PC>
2022-09-22 14:48:20 +0530

    commited

---
+++ src/main/java/org/zolando/genericUtilities/ThreadSafeClass.java
@@ -0,0 +1,42 @@
+package org.zolando.genericUtilities;
+
+import org.openqa.selenium.WebDriver;
+
+import com.aventstack.extentreports.ExtentTest;
+
+public class ThreadSafeClass {
+
+	private static ThreadLocal<ExtentTest> test=new ThreadLocal<>();
+	private static ThreadLocal<WebDriver> driver=new ThreadLocal<>();
+	private static ThreadLocal<JavaUtility> javaUtility=new ThreadLocal<>();
+	
+	//getters and setters
+	public static ExtentTest getTest() {
+		return test.get();
+	}
+
+	public static void setTest(ExtentTest sTest) {
+		test.set(sTest);
+	}
+	
+	public static WebDriver getDriver() {
+		return driver.get();
+	}
+
+	public static void setDriver(WebDriver sdriver) {
+		driver.set(sdriver);
+	}
+	
+	public static JavaUtility getJavaUtility() {
+		return javaUtility.get();
+	}
+
+	public static void setJavaUtility(JavaUtility sJavaUtility) {
+		javaUtility.set(sJavaUtility);
+	}
+	
+	
+	
+	
+	
+}
