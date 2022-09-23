70a44e3675783ffa5bc98680baa5644f626fa290
PREM <PREM@PREM-PC>
2022-09-23 13:10:14 +0530

    just commit

--- src/test/java/org/zolando/Test2.java
+++ src/test/java/org/zolando/Test2.java
@@ -1,20 +1,12 @@
 package org.zolando;
 
 import org.testng.annotations.Test;
-import org.zolando.genericUtilities.BaseClass;
-import org.zolando.genericUtilities.ThreadSafeClass;
 
-public class Test2 extends BaseClass {
+public class Test2 {
 
 	@Test
 	public void test2() {
-		ThreadSafeClass.getTest().info("log 1");
-		ThreadSafeClass.getTest().info("log 2");
-		ThreadSafeClass.getTest().info("log 3");
-		ThreadSafeClass.getTest().info("log 4");
-		ThreadSafeClass.getTest().info("log 5");
+		
 		System.out.println("im test 1");
-//		MyProjectListener listener=new MyProjectListener();
-//		listener.log1.info("log 2");
 	}
 }
